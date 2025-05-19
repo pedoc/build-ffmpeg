e6d0b3d4799287145bfd424aa6392740884abb09
pedoc <pedoc@qq.com>
2025-05-19 22:10:13 +0800

    Source /etc/profile after updating environment variables in script

--- install-arm-gcc.sh
+++ install-arm-gcc.sh
@@ -62,6 +62,7 @@ echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> /etc/profile
 echo "export CC=$INSTALL_DIR/bin/aarch64-none-linux-gnu-gcc" >> /etc/profile
 echo "export CXX=$INSTALL_DIR/bin/aarch64-none-linux-gnu-g++" >> /etc/profile
 
+source /etc/profile
 cat /etc/profile
 
 echo "gcc info"
